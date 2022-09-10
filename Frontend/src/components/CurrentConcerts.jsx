b46dad6455a4b17171cb2b6575dbdc423c643c46
GoosePerfect <efraim.kirkhorn@gmail.com>
2022-09-10 17:00:57 +0200

    VIC2-2 Fixed card and added href links

--- Frontend/src/components/CurrentConcerts.jsx
+++ Frontend/src/components/CurrentConcerts.jsx
@@ -16,17 +16,18 @@ function CurrentConcerts(){
             const artistsData = await artistsResponse.json();
 
             concertsData.forEach(concert => {
-                artistsData.forEach( artist =>  {
+
+                for(const artist of artistsData){
                     if(concert.artistId == artist.id){
-                        concert.artistName = artist.name
-                        console.log(artist.name)
+                        concert.artistName = artist.name;
+                        break;
+                    }else{
+                        concert.artistName = "artist not found"
                     }
-                });
+                };
+
                 concertList.push(concert)
             });
-
-
-
 
             updateConcertsData(concertList)
         }
@@ -46,37 +47,41 @@ function CurrentConcerts(){
 
                 {concertList.map( element => 
                     <>
-                    <div>
-                    <img
-                        className="row_poster"
-                        src={element.image}
-                        />
-                        <div class="container">
-                            <h4><b>{element.artistName}</b></h4>
-                            <h3>{element.date}</h3>
-                            <p>{element.location}</p>
-                        </div>
-                    </div>
+                        <a href={"/test?Id=" + element.id}>
+                            <div className="card">
+                            <img
+                                className="card_poster"
+                                src={element.image}
+                                />
+                                <div class="container">
+                                    <h4><b>{element.artistName}</b></h4>
+                                    <h3>{element.date}</h3>
+                                    <p>{element.location}</p>
+                                </div>
+                            </div>
+                        </a>
                     </>
                 ) }
             </card>
         </div>
-        <h3>Stream Concerts</h3>
+        <h3>Stream Concerts</h3>    
         <div className="row" >
             <card className="row_cards">
                 {concertList.map( element => 
                     <>
-                    <div>
-                    <img
-                        className="row_poster"
-                        src={element.image}
-                        />
-                        <div class="container">
-                            <h4><b>{element.artistName}</b></h4>
-                            <h3>{element.date}</h3>
-                            <p>{element.location}</p>
-                        </div>
-                    </div>
+                        <a href={"/test?Id=" + element.id}>
+                            <div className="card">
+                            <img
+                                className="card_poster"
+                                src={element.image}
+                                />
+                                <div class="container">
+                                    <h4><b>{element.artistName}</b></h4>
+                                    <h3>{element.date}</h3>
+                                    <p>{element.location}</p>
+                                </div>
+                            </div>
+                        </a>
                     </>
                 ) }
             </card>
