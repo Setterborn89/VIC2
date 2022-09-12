a5df705dd112ab922375c3431aebe204427bd18b
WazmaRas <wazma.rasouli@live.se>
2022-09-12 13:43:01 +0200

    update style

--- Frontend/src/components/Filter.jsx
+++ Frontend/src/components/Filter.jsx
@@ -1,14 +1,21 @@
 import "./Filter.css"
 function Filter(){
-    return<>
-    <ul>
-      
-  
-  <li><a className="active" href="#home">Genre</a></li>
-  <li><a href="#news">Date</a></li>
-  
-  
-</ul>
-    </>
+
+  return <>
+    <div className="navbar">
+  <a href="#home"></a>
+  <a href="#news"></a>
+  <div className="dropdown">
+    <button className="dropbtn">Genre 
+      <i className="fa fa-caret-down"></i>
+    </button>
+    <div className="dropdown-content">
+      <a href="#">pop</a>
+      <a href="#">rap</a>
+      <a href="#">hiphop 3</a>
+    </div>
+  </div> 
+</div>
+  </>
 }
 export default Filter
