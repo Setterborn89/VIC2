ce6e55ba76828c2f9a791e0c3728e495f5285a75
1kellytran <kellytran96@hotmail.com>
2022-09-14 10:39:51 +0200

    VIC-33-update-css-signIn/register

--- Frontend/src/components/SignIn.jsx
+++ Frontend/src/components/SignIn.jsx
@@ -1,66 +1,69 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-function SignIn(){
+function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+      email: email,
+      password: password,
+    };
+    console.log(data);
+    let dataResponse = await fetch("/data/login", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+    let response = await dataResponse.json();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const data = {
-          email: email,
-          password: password,
-        };
-        console.log(data);
-        let dataResponse = await fetch("/data/login", {
-          method: "post",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(data),
-        });
-        let response = await dataResponse.json();
+    response.loggedIn == true
+      ? navigate({ pathname: "/" })
+      : console.log("Could not login");
+    console.log(response);
+  };
 
-        response.loggedIn == true ? navigate({pathname: '/'}) : console.log("Could not login")        
-        console.log(response);
-      };
-
-    return <>
-
-    <div className="loginform">
-        <form onSubmit={handleSubmit}>
-          <h3>Sign In</h3>
-          <div>
-        <div className="email-login">
-            <label htmlFor="email">E-mail</label>
-            <input
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="loginform">
+        <div>
+          <h3 id="signIn">Sign In</h3>
+          <div id="signInform">
+            <div>
+              <label htmlFor="email">E-mail</label>
+              <input
                 type="email"
                 placeholder="Enter e-mail..."
                 name="emailAdress"
                 required
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-            />
-        </div>
-            <div className="password-login">
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    placeholder="At least 6 characters..."
-                    name="password"
-                    required
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
+              />
             </div>
-                <div>
-                    <button href="/" type="submit">Sign In</button>
-                </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="At least 6 characters..."
+                name="password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <button href="/" type="submit">
+                Sign In
+              </button>
+            </div>
           </div>
-        </form> 
-    </div>
-
-        </>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default SignIn;
