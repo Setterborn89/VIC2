e3618532c2fa469fb07645a556e1afc53b943a5b
My Grandin <my.grandin@gmail.com>
2022-09-15 16:04:39 +0200

    VIC2-34-update-event-details

--- Frontend/src/components/ArtistInfo.jsx
+++ Frontend/src/components/ArtistInfo.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import { GoLocation } from "react-icons/go";
+import { GoClock } from "react-icons/go";
+
 import "../css/eventdetails.css";
 
 function ArtistInfo() {
-  
   const time = "20:30";
   const [event, setEvent] = useState({});
   const { id } = useParams();
@@ -19,20 +21,22 @@ function ArtistInfo() {
         price: null,
         image: null,
         seats: null,
+        time: null,
       };
 
       let eventResponse = await fetch("/data/concerts/" + id);
       let eventResult = await eventResponse.json();
       eventData.location = eventResult.location;
       eventData.price = eventResult.price;
-      eventData.date = eventResult.date;
+      eventData.date = eventResult.date.substring(0, 10);
       eventData.artistId = eventResult.artistId;
       eventData.seats = eventResult.seats;
+      eventData.image = eventResult.image;
+      eventData.time = eventResult.date.substring(11, 16);
 
       let artistResponse = await fetch("/data/artists/" + eventData.artistId);
       let artistResult = await artistResponse.json();
       eventData.artistName = artistResult.name;
-      eventData.image = artistResult.image;
       setEvent(eventData);
     }
     loadEvent();
@@ -41,15 +45,20 @@ function ArtistInfo() {
   return (
     <>
       <div className="artistInfo">
-            <img src={event.image} />
+        <img src={event.image} />
         <div className="event-info">
           <div className="event-date">
             <span>{event.date}</span>
           </div>
           <div className="event-details">
             <h1 className="event-title">{event.artistName}</h1>
-            <a href="">{event.location}</a>
-            <h3 className="event-time">Tid: {time}</h3>
+            <a href="" className="event-location">
+              <GoLocation />
+              {event.location}
+            </a>
+            <p className="event-time">
+              <GoClock /> Tid: {event.time}
+            </p>
           </div>
         </div>
       </div>
