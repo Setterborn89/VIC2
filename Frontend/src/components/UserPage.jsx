4fee141280162d2043ea8a1436c0b9b78c439c38
Setterborn <90314260+Setterborn@users.noreply.github.com>
2022-09-15 14:41:24 +0200

    updates

--- Frontend/src/components/UserPage.jsx
+++ Frontend/src/components/UserPage.jsx
@@ -7,6 +7,12 @@ function UserPage(){
 
     const [user, updateUser] = useState({});
     const [tickets, updateTickets] = useState([]);
+    const [contentSelector, updateContentSelector] = useState(1)
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
     useEffect(() => {
         async function loadData() {
@@ -32,21 +38,114 @@ function UserPage(){
         
     }, []);
 
+    
+
+
     return (<div className="UserPage">
+        <div className="WelcomeBox">
+            <div className="WelcomeText">
+                <h1>Welcome fellow fanatic!</h1>
+            </div>
+            <div className="WelcomeBtns">
+                <a href="#" onClick={() =>updateContentSelector(1)}><h3>Tickets</h3></a>
+                <a href="#" onClick={() =>updateContentSelector(2)}><h3>Settings</h3></a>
+            </div>
+            
+        </div>
         <div  className="UserPageDisplayBox">
-            <Carousel showThumbs={false}>
-            {tickets.map(ticket => (
-                    <div key={ticket.id} className="slide">
-                        <div className="UserEventButton">
-                            <a href={"/eventdetails/" + ticket.concertId}>View concert</a>
-                            <a href={"/ticket/" + ticket.id}>View ticket</a>
-                        </div>
-                        <div className="ticketBox">
-                            <Ticket id={ticket.id}/>
+            {contentSelector==1 ? 
+                <div className="UserTickets">
+                    <Carousel showThumbs={false}>
+                    {tickets.map(ticket => (
+                            <div key={ticket.id} className="slide">
+                                <div className="UserEventButton">
+                                    <a href={"/eventdetails/" + ticket.concertId}>View concert</a>
+                                    <a href={"/ticket/" + ticket.id}>View ticket</a>
+                                </div>
+                                <div className="ticketBox">
+                                    <Ticket id={ticket.id}/>
+                                </div>
+                            </div>
+                        ))}
+                    </Carousel>
+                </div>
+                :
+                <div className="UserInfo">
+                    <div className="CurrentInfo">
+                        <form onSubmit={handleUpdateSubmit} className="personalInfoform">
+                            <div>
+                                <h3 id="update">Update account</h3>
+                                <div id="updateform">
+                                    <div>
+                                        <label htmlFor="firstName">{user.firstName}</label>
+                                        <input
+                                            type="text"
+                                            placeholder="Enter new first name"
+                                            name="firstName"
+                                            value={firstName}
+                                            onChange={(e) => setFirstName(e.target.value)}
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="lastName">{user.lastname}</label>
+                                        <input
+                                            type="text"
+                                            placeholder="Enter new last name"
+                                            name="lastName"
+                                            value={lastName}
+                                            onChange={(e) => setLastName(e.target.value)}
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="email">{user.email}</label>
+                                        <input
+                                            type="email"
+                                            placeholder="Enter new e-mail"
+                                            name="emailAdress"
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                        />
+                                    </div>
+                                    <button>Update</button>
+                                </div>
+                            </div>
+                        </form>
+                        <div>
+                            <h3 id="updatePassword">Update password</h3>
+                            <form onSubmit={handleUpdateSubmit} className="passwordForm">
+                                <div>
+                                    <label htmlFor="password">Password</label>
+                                    <input
+                                        type="password"
+                                        placeholder="Enter new password"
+                                        name="password"
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="confirmPassword">Confirm password</label>
+                                    <input
+                                        type="password"
+                                        placeholder="Confirm password"
+                                        name="confirmPassword"
+                                        onChange={(e) => setConfirmPassword(e.target.value)}
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                {password == confirmPassword ? (
+                                    <button>Update</button>
+                                ) : (
+                                    "Password doesn't match"
+                                )}
+                                </div>
+                            </form>
                         </div>
                     </div>
-                ))}
-            </Carousel>
+                </div>
+            }
         </div>
     </div>
     )
